66f3a9a930d625e8ea4a687fbd7433fe08b18b5a
djiax <djiax03@gmail.com>
2025-04-07 12:15:55 +0930

    boom

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,12 @@ export default function Home() {
 
 
       <div className='flex flex-col justify-center items-center mt-10'>
-        <div className='text-4xl border rounded-xl p-3'>The Nature of Yoga</div>
+        <div className='text-4xl border rounded-xl p-3 text-transparent bg-clip-text bg-gradient-to-r from-[#f45899] to-[#654ea3]'>The Nature of Yoga</div>
         <div className='text-center mt-8'>It is but a sacred communion between one&apos;s being and mind. To transcend one&apos;s physical limitations and reach the infinite plains of existence within.</div>
       </div>
 
       <div className='flex flex-col justify-center items-center'>
-        <div className='mt-12 text-3xl border-b'>Dimension of Yoga</div>
+        <div className='mt-12 text-3xl border-b text-transparent bg-clip-text bg-gradient-to-r from-[#f45899] to-[#654ea3]'>Dimension of Yoga</div>
         <div className='text-center mt-5'>The many forms of the art strive to promote one&apos;s overall wellbeing based their own values.</div>
         <div className='text-3xl mt-10 mb-10'>Choose Your Path</div>
         <div className='flex gap-5'>
